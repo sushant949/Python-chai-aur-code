a12fdb938fd989c4ed79ebe738a7b368e26499d4
sushant949 <sushantshukla622@gmail.com>
2025-11-28 10:28:09 +0530

    Some more examples

--- Numbers.py
+++ Numbers.py
@@ -3,3 +3,34 @@ b = 2
 c = 3
 s = a+c+b
 print(s)
+# Operations on Number 
+# We can perform multiple operations on a number like addition,multiplication,division,floor division,exponent
+
+num1 = 4
+num2 = 5
+# Addition
+print(num1 + num2)
+#  Subtraction
+print(num1-num2)
+# Division
+print(num1/num2)
+#Floor Division
+print(num1//num2)
+# multiplication
+print(num1*num2)
+# Exponent(power)
+print(num1**num2)
+# Remainder(Modulus)
+print(num1 % num2)
+print(num2 % num1)
+
+# The output of above code till line 25 will be
+# (6
+# 9
+# -1
+# 0.8
+# 0
+# 20
+# 1024
+# 4
+# 1)
