39a306275a0c3887de5f921c1402ac7d11b8e3e3
sushant949 <sushantshukla622@gmail.com>
2025-11-28 10:38:44 +0530

    your message

--- hello_chai.py
+++ hello_chai.py
@@ -12,4 +12,17 @@ chaiPeLO('Normal Chai')
 chai_one = "Masala Tea"
 chai_two = "ginger Tea"
 chai_three = "lemon Tea"
+# Printing each caharacters 
+s = "Sushant Shukla"
+for c in s:
+    print(c)
+
 # this is a comment 
+
+# Inner working of python
+# Whenever we write code in python.The python compiler converts it into byte code these byte code gets stored in .pyc files these files are not
+# visible in every cases whenever we import something than these files are visible to us 
+# Python Virtual Machine(PVM):- Code loop to iterate byte code.Runtime Engine.Also known as python interpreter.Byte code is not machine code
+# Mutable v/s Immutable:-
+# Mutable means that can be changed while immutable means that can not be changed.
+# Whenever we make any variables than in python the reference gets stored in memory not the variable name means there is a type of reference not for variable
